7f2eb44a41b86f5647d6672a64391bc7d66f7d70
Arlin Grijalba <Arlin Grijalba>
2017-03-28 11:11:25 -0700

    First commit

---
+++ TopSpot/Controllers/TopSpotController.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Web.Http;
+
+namespace TopSpot.Controllers
+{
+    public class TopSpotController : ApiController
+    {
+        [HttpGet, Route("api/getTopSpots")]
+        public IHttpActionResult GetAllTopSpot()
+        {
+            var topspots = File.ReadAllText("C:\\Users\\Arlin\\Documents\\Visual Studio 2013\\Projects\\OriginCodeAcademy\\TopSpot\\topsSpots.json");
+            return Ok(topspots);
+        }
+    }
+}
